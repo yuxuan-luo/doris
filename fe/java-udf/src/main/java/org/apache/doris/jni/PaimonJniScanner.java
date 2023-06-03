fe7bebcdf81da7377fcfcc807d731d07b5db7b49
hugoluo <hugoluo@tencent.com>
2023-06-03 10:51:34 +0800

    [Feature](multi-catalog) paimon catalog scan

--- fe/java-udf/src/main/java/org/apache/doris/jni/PaimonJniScanner.java
+++ fe/java-udf/src/main/java/org/apache/doris/jni/PaimonJniScanner.java
@@ -46,6 +46,7 @@ import org.apache.paimon.table.source.TableRead;
 import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Map;
 
 
@@ -104,7 +105,8 @@ public class PaimonJniScanner extends JniScanner {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        ReadBuilder readBuilder = table.newReadBuilder();
+        ReadBuilder readBuilder = table.newReadBuilder()
+                                    .withProjection(Arrays.stream(ids).mapToInt(Integer::parseInt).toArray());
         TableRead read = readBuilder.newRead();
         reader = read.createReader(paimonInputSplit.split());
     }
@@ -118,7 +120,7 @@ public class PaimonJniScanner extends JniScanner {
     protected int getNext() throws IOException {
         int rows = 0;
         try {
-            RecordReader.RecordIterator batch;
+            RecordReader.RecordIterator<InternalRow> batch;
             while ((batch = reader.readBatch()) != null) {
                 Object record;
                 while ((record = batch.next()) != null) {
